025a94bd1d2a43ebc8e8fb86f9008ca9c8428128
Juliors1 <user@email.com>
2020-12-04 02:02:19 -0500

    compile schema into model

--- models/flight.js
+++ models/flight.js
@@ -1,11 +1,21 @@
 // define the schema
 const mongoose = require("mongoose");
-const Schema = mongoose.Schema
+const Schema = mongoose.Schema;
 
 // compile into a model
-const flightSchema = new Schema ({
- airline: {type:String,enum:['American', 'United', 'COPA']},
-
-})
+const flightSchema = new Schema({
+  airline: { type: String, enum: ["American", "United", "Delta"] },
+  airport: {
+    type: String,
+    enum: ["AUS", "DEN", "JFK", "DFW", "PHX"],
+    default: "JFK",
+  },
+  flightNo: { type: Number, required: true, min: 10, max: 9999 },
+  departs: {
+    type: Date,
+    default: () => Date.now() + 365 * 24 * 60 * 60 * 1000,
+    // + 365 days * hours * minutes * seconds * milliseconds
+  },
+});
 
 //export model
