e6c6130269f32c984a093b140d3f5bed569ae677
Juliors1 <user@email.com>
2020-12-04 03:44:39 -0500

    add info ito index.ejs in flights

--- controllers/flights.js
+++ controllers/flights.js
@@ -12,5 +12,15 @@ function newFlight(req, res){
 }
 
 function create(req, res){
-
+for(let key in req.body){
+  if(req.body[key] === '') delete req.body[key]
+}
+const flight = new Flight(req.body)
+flight.save(function(err) {
+  if(err) {
+    return res.render('flights/new',{err:err, title: 'Add Flight'})
+  }
+  console.log(flight);
+  res.redirect('/flights',{title: 'Flights'})
+})
 }
