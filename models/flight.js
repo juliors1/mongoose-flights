6129351cd793381796067619b9eb8ba9718fcc43
Juliors1 <user@email.com>
2020-12-04 01:48:22 -0500

    define schema

--- models/flight.js
+++ models/flight.js
@@ -1,4 +1,11 @@
+// define the schema
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema
 
+// compile into a model
+const flightSchema = new Schema ({
+ airline: {type:String,enum:['American', 'United', 'COPA']},
 
+})
 
-
+//export model
