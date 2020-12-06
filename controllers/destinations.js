9cfce365283dfb3cd91ddc2d5125c5db634a2bb0
Juliors1 <user@email.com>
2020-12-06 00:16:28 -0500

    add delete function

--- controllers/destinations.js
+++ controllers/destinations.js
@@ -1,28 +1,35 @@
-const Destination = require('../models/destination')
-const Flight = require('../models/flight')
+const Destination = require("../models/destination");
+const Flight = require("../models/flight");
 
 module.exports = {
-    new: newDestination,
-    create,
-    addToDestinations
+  new: newDestination,
+  create,
+  addToDestinations,
+  delete: deleteDestination,
+};
+
+function newDestination(req, res) {
+  Destination.find({}, function (err, destinations) {
+    res.render("destinations/new", { title: "Add Destination", destinations });
+  });
+}
+function create(req, res) {
+  Destination.create(req.body, function (err, destinations) {
+    res.redirect("/destinations/new");
+  });
 }
 
-function newDestination(req, res){
-    Destination.find({}, function(err, destinations) {
-        res.render('destinations/new', {title: 'Add Destination', destinations})
-    })
-}
-function create(req, res){
-    Destination.create(req.body, function(err, destinations){
-        res.redirect('/destinations/new')
-    })
+function addToDestinations(req, res) {
+  Flight.findById(req.params.id, function (err, flight) {
+    flight.airport.push(req.body.destinationId);
+    flight.save(function (err) {
+      res.redirect(`/flights/${flight._id}`);
+    });
+  });
 }
 
-function addToDestinations(req, res){
-    Flight.findById(req.params.id, function(err, flight) {
-        flight.airport.push(req.body.destinationId);
-        flight.save(function(err) {
-          res.redirect(`/flights/${flight._id}`);
-        });
-      });
+function deleteDestination(req, res) {
+  Destination.findByIdAndDelete(req.params.id)
+  console.log('ERRORS')
+  res.redirect('/destinations/new')
 }
--- routes/destinations.js
+++ routes/destinations.js
@@ -5,5 +5,6 @@ const destinationsCtrl = require('../controllers/destinations')
 router.get('/destinations/new', destinationsCtrl.new)
 router.post('/destinations', destinationsCtrl.create)
 router.post('/flights/:id/destinations', destinationsCtrl.addToDestinations);
+router.delete('/destinations/:id', destinationsCtrl.delete)
 
 module.exports = router
